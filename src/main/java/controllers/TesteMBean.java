0cccf7dddfa7281d8c4ce24fcc192abe61196cc6
rafaelleme100 <42623246+rafaelleme100@users.noreply.github.com>
2018-08-28 10:38:27 -0300

    Initial commit

---
+++ src/main/java/controllers/TesteMBean.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import javax.faces.bean.ManagedBean;
+
+public class TesteMBean {
+	
+	private String nome = "Rafael";
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+}
